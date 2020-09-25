3966ebef542e19b14855e54e1d276193f3c8c0b8
Mohamed Toure <demahom.toure@gmail.com>
2020-09-25 21:53:09 +0200

    validation rules added for room reservation

--- src/main/java/com/safarihotel/controller/ReservationController.java
+++ src/main/java/com/safarihotel/controller/ReservationController.java
@@ -6,9 +6,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -27,7 +30,13 @@ public class ReservationController {
     }
 
     @PostMapping
-    String create(Reservation reservation) {
+    String create(@Valid Reservation reservation, Errors errors) {
+        log.info("Form validation");
+        if (errors.hasErrors()) {
+            log.info("Form contains errors");
+            return "reservation";
+        }
+        log.info("Validation done!");
         log.info("Creating reservation");
         Reservation results = this.reservationService.create(reservation);
         log.info("Reservation created");
--- src/main/java/com/safarihotel/model/Reservation.java
+++ src/main/java/com/safarihotel/model/Reservation.java
@@ -1,11 +1,26 @@
 package com.safarihotel.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.*;
+import java.util.Date;
 
 @Data
 public class Reservation {
     private Long id;
+
+    @NotEmpty(message = "Le prénom est obligatoire")
+    @NotBlank(message = "Format de prénom invalide")
+    @Pattern(regexp = "((?=[A-Za-z])(?![_\\-]).)*$", message = "Le prénom contient des caractères non autorisés")
     private String firstName;
+
+    @NotEmpty(message = "Le nom est obligatoire")
+    @NotBlank(message = "Format de nom invalide")
+    @Pattern(regexp = "((?=[A-Za-z])(?![_\\-]).)*$", message = "Le nom contient des caractères non autorisés")
     private String lastName;
+
+    @NotEmpty(message = "Email obligatoire")
+    @Email(message = "Email invalide")
     private String email;
 }
