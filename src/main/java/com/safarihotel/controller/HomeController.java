174dee7d253c26639313fc672ba8e231175af3ad
Mohamed Toure <demahom.toure@gmail.com>
2020-09-27 20:44:48 +0200

    rooms added in index list

--- src/main/java/com/safarihotel/controller/HomeController.java
+++ src/main/java/com/safarihotel/controller/HomeController.java
@@ -1,12 +1,31 @@
 package com.safarihotel.controller;
 
+import com.safarihotel.model.Room;
+import com.safarihotel.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
+    private final RoomService roomService;
+
+    @Autowired
+    public HomeController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
     @GetMapping("/")
-    String getHomePage() {
+    String getHomePage(Model model) {
+        List<Room> rooms = roomService.findAll();
+
+        model.addAttribute("rooms", rooms.stream().limit(3).collect(Collectors.toList()));
+
         return "index";
     }
 }
